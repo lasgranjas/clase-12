b8a338c9eb14eb19e60187aa1c5f67c9afcc6273
lasgranjas <78686390+lasgranjas@users.noreply.github.com>
2021-04-27 21:41:30 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+game.setDialogFrame(img`
+    a a a 3 3 3 3 3 3 3 3 3 a a a 
+    a a a 3 3 3 3 3 3 3 3 3 a a a 
+    a a a 3 3 3 3 3 3 3 3 3 a a a 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    3 3 3 9 9 9 9 9 9 9 9 9 3 3 3 
+    a a a 3 3 3 3 3 3 3 3 3 a a a 
+    a a a 3 3 3 3 3 3 3 3 3 a a a 
+    a a a 3 3 3 3 3 3 3 3 3 a a a 
+    `)
+game.setDialogCursor(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `)
+game.setDialogTextColor(10)
+game.showLongText("HOLA, CÓMO ESTAS?", DialogLayout.Bottom)
